13d5f20681b61819a4cc34640170d312247aa82d
CioazaIoan <cioazai@yahoo.com>
2026-01-09 08:11:40 +0200

    sdgg

--- script.js
+++ script.js
@@ -48,3 +48,51 @@ function scadeDinArray(numere, numarDeScazut) {
   return numere.map((num) => num - numarDeScazut);
 }
 console.log(scadeDinArray([10, 20, 30], 5));
+
+//Scăderea numerelor dintr-un string:
+
+function scadeNumereString(input) {
+  return input
+    .split("-")
+    .map(Number)
+    .reduce((total, num) => total - num);
+}
+console.log(scadeNumereString("20-5-3"));
+// Scăderea a două numere imutabile (folosind const):
+
+function scadeImutabile(num1, num2) {
+  const rezultat = num1 - num2;
+  return rezultat;
+}
+console.log(scadeImutabile(15, 7));
+
+//Scăderea valorilor dintr-un obiect:
+
+function scadeDinObiect(obiect) {
+  return obiect.valoare1 - obiect.valoare2;
+}
+console.log(scadeDinObiect({ valoare1: 50, valoare2: 20 }));
+
+// Scăderea numerelor în ordine descrescătoare:
+
+function scadeDescrescator(numere) {
+  return numere.sort((a, b) => b - a).reduce((total, num) => total - num);
+}
+console.log(scadeDescrescator([5, 2, 8]));
+
+// Scăderea pe baza unui criteriu:
+
+function scadeDaca(numere, criteriu) {
+  return numere
+    .filter((num) => num > criteriu)
+    .reduce((total, num) => total - num, 0);
+}
+console.log(scadeDaca([10, 20, 30, 40], 15));
+
+// Scăderea unui procent dintr-un număr:
+
+function scadeProcent(numar, procent) {
+  const scadere = (procent / 100) * numar;
+  return numar - scadere;
+}
+console.log(scadeProcent(100, 20));
